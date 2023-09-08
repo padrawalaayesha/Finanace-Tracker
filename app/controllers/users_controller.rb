cdc1154965727cf223883492ecde72205676d239
Ayesha Padrawala <apadrawala5@gmail.com>
2023-09-08 14:53:01 +0530

    Add friend and stop following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,11 @@ class UsersController < ApplicationController
   end
   def search
     if params[:friend].present?
-        @friend = params[:friend]
-        if @friend
+        @friends = User.search(params[:friend])
+        @friends = current_user.except_current_user(@friends)
+        if @friends
           respond_to do |format|
-            format.js{ render partial: 'users/rfriend_esult'}
+            format.js{ render partial: 'users/friend_result'}
           end  
         else
             respond_to do |format|
